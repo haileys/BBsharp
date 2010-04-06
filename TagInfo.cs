70562bcba9975c6f01a551c7518d9aaa6aa99fe9
charliesome <charlie@charliesomerville.com>
2010-04-06 16:22:37 +1000

    First usable revision.

---
+++ TagInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BBSharp
+{
+    public class TagInfo
+    {
+        public string bbtag { get; set; }
+        public string etag { get; set; }
+
+        public TagInfo(string bbtag, string etag)
+        {
+            this.bbtag = bbtag;
+            this.etag = etag;
+        }
+    }
+}
