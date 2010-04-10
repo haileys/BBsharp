eef9aaf654d26d275e24cdcfd597d6e091ff5dfa
charliesome <charlie@charliesomerville.com>
2010-04-10 18:47:41 +1000

    Added bbsharp.Easy namespace with a single extension method for converting a string of BBCode to a string of HTML.

---
+++ Renderers/Html/Easy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace bbsharp.Renderers.Html
+{
+    public static partial class HtmlRenderer
+    {
+        public static string BbToHtml(string BBCode)
+        {
+            return BBCodeDocument.Load(BBCode, false, new string[] { "hr" }).ToHtml(false);
+        }
+    }
+}
